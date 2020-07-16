d9de95d4fa391f027808787e866532ec03711116
Sagynbek <kenzhebaevsagynbek@gmail.com>
2020-07-16 22:39:50 +0300

    ui using query module service

--- client/src/comments/CommentList.js
+++ client/src/comments/CommentList.js
@@ -1,17 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios'
+import React from 'react';
 
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-  
-  const fetchComments = async () => {
-    const result = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
-    setComments(result.data);
-  }
-
-  useEffect(() => {
-    fetchComments();
-  }, []);
+const CommentList = ({ comments }) => {
 
   const commentContainer = comments.map(comment => (
     <li key={comment.id}>
--- client/src/posts/PostList.js
+++ client/src/posts/PostList.js
@@ -8,7 +8,7 @@ const PostList = () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const result = await axios.get("http://localhost:4000/posts");
+    const result = await axios.get("http://localhost:4002/posts");
     setPosts(result.data);
   }
 
@@ -22,7 +22,7 @@ const PostList = () => {
       <div key={post.id} className="card" style={{ width: "30%", marginBottom: "20px" }}>
         <div className="card-body">
           <h3>{post.title}</h3>
-          <CommentList postId={post.id} />
+          <CommentList comments={post.comments} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
