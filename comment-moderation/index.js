8b81f14011b78974c0ac23c844ae1bf320b538bc
Sagynbek <kenzhebaevsagynbek@gmail.com>
2020-07-16 23:51:26 +0300

    CommentModeration added

---
+++ comment-moderation/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "CommentCreated") {
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+    await axios.post('http://localhost:4005/events', {
+      type: "CommentModerated",
+      data: {
+        ...data,
+        status,
+      }
+    });
+  }
+
+  res.send({});
+});
+
+app.listen(4003, () => {
+  console.log("App listening on port 4003");
+});
