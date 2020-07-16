6942ca6029518dac3e28c394e3ccf981eeae9ec0
Sagynbek <kenzhebaevsagynbek@gmail.com>
2020-07-16 22:27:58 +0300

    Comment/Post modules listen for events

--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
+
 const app = express();
 
 
@@ -9,11 +11,13 @@ const posts = {};
 
 app.use(bodyParser.json());
 app.use(cors());
+
+
 app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
 
@@ -21,8 +25,20 @@ app.post("/posts", (req, res) => {
     id, title
   };
 
+  await axios.post('http://localhost:4005/events', {
+    type: "PostCreated",
+    data: posts[id],
+  });
+
   res.status(201).send(posts[id]);
 });
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  console.log("Received event", type);
+  res.send({});
+})
 
 app.listen(4000, () => {
   console.log("Listening on port 4000");
